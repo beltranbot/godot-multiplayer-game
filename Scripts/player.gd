bdb5e1ce62423cc0d178858e7d75d6412693a868
beltranbot <beltranbot@gmail.com>
2025-08-19 23:23:46 -0500

    added player characater horizontal movement

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-var movement_speed: int
+var movement_speed: int = 600
 var jump_force: int = 1000
 var gravity: int = 2800
 var direction: float
@@ -11,5 +11,12 @@ func _physics_process(delta: float) -> void:
 
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y -= jump_force
+
+	direction = Input.get_axis("move_left", "move_right")
+
+	if direction: 
+		velocity.x = direction * movement_speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, movement_speed)
 
 	move_and_slide()
