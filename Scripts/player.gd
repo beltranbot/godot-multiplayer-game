bd75141b90aff11d19c376138e172f148d5a52bf
beltranbot <beltranbot@gmail.com>
2025-08-19 23:11:43 -0500

    added jump functionality

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -6,7 +6,10 @@ var gravity: int = 2800
 var direction: float
 
 func _physics_process(delta: float) -> void:
+	if not is_on_floor():
+		velocity.y += gravity * delta
 
-    if not is_on_floor():
-        velocity.y += gravity * delta
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y -= jump_force
 
+	move_and_slide()
