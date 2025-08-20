dfa63fd0832614d42e91d00c70b76937b4f55979
beltranbot <beltranbot@gmail.com>
2025-08-19 23:49:01 -0500

    added animations to player character

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -4,7 +4,7 @@ var movement_speed: int = 600
 var jump_force: int = 1000
 var gravity: int = 2800
 var direction: float
-@onready var sprite: Sprite2D = $Sprite2D
+@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
@@ -15,11 +15,16 @@ func _physics_process(delta: float) -> void:
 
 	direction = Input.get_axis("move_left", "move_right")
 
-	if direction: 
+	if direction:
 		velocity.x = direction * movement_speed
+		sprite.play("Walk")
 	else:
 		velocity.x = move_toward(velocity.x, 0, movement_speed)
+		sprite.play("Idle")
 
 	sprite.flip_h = sprite.flip_h if not direction else direction < 0
+
+	if not is_on_floor():
+		sprite.play("Jump")
 
 	move_and_slide()
