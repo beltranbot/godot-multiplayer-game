7fd6c60af58f4e22c6ad922c964517c51a7c807e
beltranbot <beltranbot@gmail.com>
2025-08-28 00:01:31 -0500

    added game finished node

---
+++ Scripts/ui.gd
@@ -0,0 +1,18 @@
+extends Control
+
+
+func game_finished(winner_index: String) -> void:
+	$GameFinished.show()
+	$GameFinished/WinnerLabel.text = "Player " + winner_index + " Wins!"
+	# pause the game
+	get_tree().paused = true
+
+
+func _on_play_again_button_pressed() -> void:
+	get_tree().reload_current_scene()
+	get_tree().paused = false
+
+
+func _on_home_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/menu.tscn")
+	get_tree().paused = false
